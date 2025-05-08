4b0f29acc1a22000edfa8e53b953dc513500716d
Samin Haque <saminhaque213@gmail.com>
2025-05-08 21:28:33 +0600

    B: rd1

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,6 @@ python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 deactivate
